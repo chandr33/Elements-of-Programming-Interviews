4a7332fbb2c0e80b07b1536f81e795248ac0cc11
chandr33 <rochak.chandra96@gmail.com>
2020-07-14 06:57:21 -0500

    Implemented Prime Sieve

--- epi_judge_java/epi/SpreadsheetEncoding.java
+++ epi_judge_java/epi/SpreadsheetEncoding.java
@@ -1,12 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.lang.Math;
 public class SpreadsheetEncoding {
   @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
+  private static int calcOffset(int n) {
+    if (n == 1)
+      return 0;
+    int sum = 0;
+    for (int i = 2; i <= n; i++)
+      sum += Math.pow(26, i-1);
+    return sum;
+  }
   public static int ssDecodeColID(final String col) {
-    // TODO - you fill in here.
-    return 0;
+    if (col == null)
+      return 0;
+    char[] str = col.toCharArray();
+    int sum = 1, n = str.length;
+    for (int i = 0; i < n; i++) {
+      int coeff = str[i] - 'A';
+      int multiplier = n - i - 1;
+      sum += coeff * Math.pow(26, multiplier);
+    }
+    return calcOffset(n) + sum;
   }
 
   public static void main(String[] args) {
